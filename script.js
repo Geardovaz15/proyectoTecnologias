550b0f9100d16ddf53a6b34dc50a1927d2b1fc06
Geardovaz15 <163466586+Geardovaz15@users.noreply.github.com>
2024-03-21 19:30:35 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,43 @@
 function iniciar(){
   location.href="preguntas.html";
 }
+
+let respuestas=[];
+
+function resultado(pregunta, respuesta){
+
+  respuestas[pregunta]= respuesta.value;
+
+}
+
+function calcular(){
+    let humanidades=0;
+    let ciencias=0;
+    let psyco=0;
+
+  
+    for(i=0;i<=4;i++){
+    switch(respuestas[i]){
+      case 1:
+        humanidades++;
+        break;
+      case 2:
+        ciencias ++;
+        break;
+      case 3:
+        psyco++;
+        break;
+      default:
+        console.log("responde no seas wey ${i}");
+    }
+  }
+  if(humanidades>ciencias && humanidades>psyco){
+    location.href="humani.html";
+  }
+  else if(ciencias>humanidades && ciencias>psyco){
+    location.href="cienc.html";
+  }
+  else if(psyco>ciencias && psyco>humanidades){
+    location.href="psyco.html";
+   }
+}
