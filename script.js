0d63f001640a93f7171ae4a3012f75f7d76c89ac
Geardovaz15 <163466586+Geardovaz15@users.noreply.github.com>
2024-05-18 15:33:02 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,220 +1,165 @@
-//funciones para referir a links
+///funciones para referir a links
 function iniciar(){
-  llevar();
+  location.href="preguntas1.html";
 }
-
 function home(){
   location.href="index.html";
 }
-
 function cuceiF(){
   location.href="https://www.facebook.com/udegcucei/?locale=es_LA";
 }
 function cuceiW(){
   location.href="http://www.cucei.udg.mx/";
 }
-
 function cuceaF(){
   location.href="https://www.facebook.com/cucea.oficial/?locale=es_LA";
 }
 function cuceaW(){
   location.href="https://www.cucea.udg.mx/";
 }
-
 function cucadF(){
   location.href="https://es-la.facebook.com/CUAAD/";
 }
 function cucadW(){
   location.href="https://cuaad.udg.mx/";
 }
-
 function cucsF(){
   location.href="https://www.facebook.com/CUCSUdeG/?locale=es_LA";
 }
 function cucsW(){
   location.href="https://www.cucs.udg.mx/";
 }
-
 function cucshW(){
   location.href="http://www.cucsh.udg.mx/";
 }
-
 function udgF(){
   location.href="https://www.facebook.com/udg.mx/?locale=es_LA";
 }
 function udgW(){
   location.href="https://www.udg.mx/es";
 }
-
-
 //funciones para calcular resultados
-let respuesta=0;
-let totales=[];
-let pika=[];
-
-function resultado(pregunta, respues){
-  respuesta= respues.value;
+let respuestas=[];
+function resultado(pregunta, respuesta){
+  respuestas[pregunta]= respuesta.value;
 }
-
 function calcular(){
-    
-    switch(respuesta){
+    let Der=0;
+    let Izq=0;
+  
+    for(i=0;i<=4;i++){
+    switch(respuestas[i]){
       case "1":
-        if(totales[0]==null){
-          totales[0]=0;
-        }
-        totales[0]+=1;
+        Izq++;
         break;
       case "2":
-        if(totales[1]==null){
-          totales[1]=0;
-        }
-        totales[1]+=1;
-        break;
-       case "3":
-        if(totales[2]==null){
-          totales[2]=0;
-        }
-        totales[2]+=1;
-        break;
-       case "4":
-        if(totales[3]==null){
-          totales[3]=0;
-        }
-        totales[3]+=1;
-        break;
-       case "5":
-        if(totales[4]==null){
-          totales[4]=0;
-        }
-        totales[4]+=1;
-        break;
-       case "6":
-        if(totales[5]==null){
-          totales[5]=0;
-        }
-        totales[5]+=1;
-        break;
-       case "7":
-        if(totales[6]==null){
-          totales[6]=0;
-        }
-        totales[6]+=1;
-        break;
-       case "8":
-        if(totales[7]==null){
-          totales[7]=0;
-        }
-        totales[7]+=1;
-        break;
-       case "9":
-        if(totales[8]==null){
-          totales[8]=0;
-        }
-        totales[8]+=1;
-        break;
-       case "10":
-        if(totales[9]==null){
-          totales[9]=0;
-        }
-        totales[9]+=1;
+        Der++;
         break;
       default:
-        console.log("En teoria no tiene que salir eso. Hay un error en value o en la respuesta");
-    }
-    
-}
-
-
-function elegir(){
-  let sig=0;
-  let pre= "preguntas";
-  sig=Math.floor(Math.random() *(12 - 1) + 1);
-  pre=pre.concat(sig);
-  return pre;
-}
-
-function decidir(){
-  let mayor=0;
-  let posMayor=0;
-  for (let i = 1; i < totales.length; i++) {
-    if (totales[i] > mayor) {
-        mayor = totales[i];
-        posMayor = i;
+        console.log("al chile no se porque imprime esto");
     }
   }
-    switch(posMayor){
-      case 0:
-        location.href="quim.html";
-        break;
-      case 1:
-        location.href="arte.html";
-        break;
-      case 2:
-        location.href="cont.html";
-        break;
-      case 3:
-        location.href="dere.html";
-        break;
-      case 4:
-        location.href="fisi.html";
-        break;
-      case 5:
+  if(Der>Izq){
+    return "D";
+  }
+  else if(Izq>Der){
+    return "I";
+  }
+}
+function llevar(actual){
+  switch(actual){
+    case 1:
+      if (calcular()=="I"){
+        location.href="preguntas2.html";
+        }
+      else{
+        location.href="preguntas3.html";
+        }
+      break;
+    case 2:
+      if (calcular()=="I"){
+        location.href="preguntas4.html";
+      }
+      else{
+        location.href="preguntas9.html";
+      }
+      break;
+    case 3:
+      if (calcular()=="I"){
+        location.href="preguntas7.html";
+      }
+      else{
+        location.href="preguntas8.html";
+      }
+      break;
+    case 4:
+      if (calcular()=="I"){
+        location.href="preguntas5.html";
+      }
+      else{
+        location.href="preguntas6.html";
+      }
+      break;
+    case 5:
+      if (calcular()=="I"){
         location.href="infinc.html";
-        break;
-       case 6:
-        location.href="medi.html";
-        break;
-       case 7:
-        location.href="merc.html";
-        break;
-       case 8:
+      }
+      else{
         location.href="robele.html";
-        break;
-       case 9:
+      }
+      break;
+    case 6:
+      if (calcular()=="I"){
+        location.href="mate.html";
+      }
+      else{
+        location.href="fisi.html";
+      }
+      break;
+    case 7:
+      if (calcular()=="I"){
+        location.href="merc.html";
+      }
+      else{
+        location.href="dere.html";
+      }
+      break;
+    case 8:
+      if (calcular()=="I"){
         location.href="turi.html";
-        break;
-      default:
+      }
+      else{
+        location.href="cont.html";
+      }
+      break;
+      case 9:
+      if (calcular()=="I"){
+        location.href="preguntas10.html";
+      }
+      else{
+        location.href="preguntas11.html";
+        
+      }
+      break;
+      case 10:
+      if (calcular()=="I"){
+        location.href="medi.html";
+      }
+      else{
+        location.href="quim.html";
+      }
+
+      break;
+      case 11:
+      if (calcular()=="I"){
         location.href="nodef.html";
-  }
-}
-
-function llevar(){
-  let j=0;
-  llev=elegir();
-  console.log(pika);
-  console.log(llev);
-  while(checar(llev)!=0){
-    llev=elegir();
-  }
-  while(j!=pika.length){
-    j++
-  }
-  pika[j+1]=llev;
-  console.log(totales);
-  console.log(llev);
-  console.log(pika);
-  llev=llev.concat(".html");
-   
-  if(pika.length==3){
-    decidir();
-  }
-  else{
-    location.href=llev;
-  }
-}
-
-function checar(llev){
-  let fla=0;
-  for(i=0;i<=pika.length;i++){
-    if(pika[i]==llev){
-      fla=1;
-    }
-  }
-  if(fla==1){
-    return 1;
-    }
-  else{
-    return 0;
+      }
+      else{
+        location.href="arte.html";
+      }
+     break;
+    default:
+      console.log("No existe el numero que mandaste de referencia");
+      break;
   }
 }
